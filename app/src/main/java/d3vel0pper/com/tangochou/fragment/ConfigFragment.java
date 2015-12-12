fcf8c05076b37d6eb8b4b913e04311cdd8ac5d62
D3vel0pper <toamaindex@gmail.com>
2015-12-12 15:51:26 +0900

    manage config display

--- app/src/main/java/d3vel0pper/com/tangochou/fragment/ConfigFragment.java
+++ app/src/main/java/d3vel0pper/com/tangochou/fragment/ConfigFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import d3vel0pper.com.tangochou.R;
 
@@ -12,14 +13,18 @@ import d3vel0pper.com.tangochou.R;
  * Created by taka-dhu on 2015/12/09.
  */
 public class ConfigFragment extends Fragment {
+    ListView listView;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.configfragment,container,false);
+        listView = (ListView)v.findViewById(R.id.Setting_List);
+        //add onItemClicked
         return v;
     }
 }
