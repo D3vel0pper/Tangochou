a81c451dc7ad518f5b97c7e3a69bbecb4ec75949
D3vel0pper <toamaindex@gmail.com>
2015-12-15 13:41:58 +0900

    add Resource

---
+++ app/src/main/java/d3vel0pper/com/tangochou/commons/ConfigContents.java
@@ -0,0 +1,9 @@
+package d3vel0pper.com.tangochou.commons;
+
+/**
+ * Created by taka-dhu on 2015/12/15.
+ * have an image and string for config list
+ */
+public class ConfigContents {
+    String name;
+}
