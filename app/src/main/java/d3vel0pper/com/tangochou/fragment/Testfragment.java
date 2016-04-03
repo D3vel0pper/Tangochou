80fdb8b2976de20a620112cf3157766cf3b91cbe
D3vel0pper <toamaindex@gmail.com>
2016-04-03 10:46:35 +0900

    added VP

--- app/src/main/java/d3vel0pper/com/tangochou/fragment/Testfragment.java
+++ app/src/main/java/d3vel0pper/com/tangochou/fragment/Testfragment.java
@@ -10,15 +10,7 @@ import android.view.ViewGroup;
 
 import d3vel0pper.com.tangochou.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Fragment2.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Fragment2#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Fragment2 extends Fragment {
+public class Testfragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -28,17 +20,18 @@ public class Fragment2 extends Fragment {
     private String mParam1;
     private String mParam2;
 
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment2.
+     * @return A new instance of fragment Testfragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Fragment2 newInstance(String param1, String param2) {
-        Fragment2 fragment = new Fragment2();
+    public static Testfragment newInstance(String param1, String param2) {
+        Testfragment fragment = new Testfragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -46,7 +39,7 @@ public class Fragment2 extends Fragment {
         return fragment;
     }
 
-    public Fragment2() {
+    public Testfragment() {
         // Required empty public constructor
     }
 
@@ -63,9 +56,7 @@ public class Fragment2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment2, container, false);
-
-        return v;
+        return inflater.inflate(R.layout.testfragment, container, false);
     }
 
     @Override
@@ -77,5 +68,7 @@ public class Fragment2 extends Fragment {
     public void onDetach() {
         super.onDetach();
     }
+
+
 
 }
